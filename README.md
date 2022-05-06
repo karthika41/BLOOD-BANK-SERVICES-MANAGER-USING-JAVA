aa5d55f0aafdcff42e2d7093a93f8f98aa70827b
karthika41 <sjkarthika41@gmail.com>
2022-05-06 21:43:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BLOOD-BANK-SERVICES-MANAGER-USING-JAVA
+ Blood bank management system using java
---
+++ src/com/karthika/bloodbank/Start.java
@@ -0,0 +1,12 @@
+package com.karthika.bloodbank;
+
+import com.karthika.bloodbank.Home.Homee;
+
+public class Start
+{
+	public static void main(String args[]) throws Exception
+	{
+		Homee h = new Homee();
+		h.setVisible(true);
+	}
+}
